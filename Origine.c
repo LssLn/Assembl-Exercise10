20c4a69b801afd7fa10f6728c10baa9937b1b829
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-02-12 23:58:30 +0100

    Update Origine.c

--- Origine.c
+++ Origine.c
@@ -1,19 +1,19 @@
-;# int processa(char *st, char val, int d)
-;# { int j,cnt, p;
-;# cnt=0;
-;# for(j=0;j<d;j++)
-;# if(st[j]<val)
-;# cnt++;
-;# return cnt;
-;# }
-;# main() {
-;# char ST[16];
-;# int i,pos,conta;
-;# for(i=0;i<4;i++) {
-;# printf("Inserisci una stringa \n");
-;# scanf("%s",ST);
-;# pos=strlen(ST)/2;
-;# conta= processa(ST,ST[pos],strlen(ST));
-;# printf(" Valore= %d \n",conta);
-;# }
-;# }
+int processa(char *st, char val, int d)
+{ int j,cnt, p;
+cnt=0;
+for(j=0;j<d;j++)
+if(st[j]<val)
+cnt++;
+return cnt;
+}
+main() {
+char ST[16];
+int i,pos,conta;
+for(i=0;i<4;i++) {
+printf("Inserisci una stringa \n");
+scanf("%s",ST);
+pos=strlen(ST)/2;
+conta= processa(ST,ST[pos],strlen(ST));
+printf(" Valore= %d \n",conta);
+}
+}
