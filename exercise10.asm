454ea3905c1605b60c07cf95b03c508a620c81d0
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-02-16 21:54:32 +0100

    Update exercise10.asm

--- exercise10.asm
+++ exercise10.asm
@@ -1,23 +1,3 @@
-;# int processa(char *st, char val, int d)
-;# { int j,cnt, p;
-;# cnt=0;
-;# for(j=0;j<d;j++)
-;# if(st[j]<val)
-;# cnt++;
-;# return cnt;
-;# }
-;# main() {
-;# char ST[16];
-;# int i,pos,conta;
-;# for(i=0;i<4;i++) {
-;# printf("Inserisci una stringa \n");
-;# scanf("%s",ST);
-;# pos=strlen(ST)/2;
-;# conta= processa(ST,ST[pos],strlen(ST));
-;# printf(" Valore= %d \n",conta);
-;# }
-;# }
-
 .data
 ST: .space 16 ;# ST[16]
 pos: .space 8 ;# INUTILE? int pos, conta la metto come arg di printf
